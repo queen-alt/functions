d285da8e5a6327ec062c60d8b1354ebaaeb64c53
Shukriya Abduselam <shukriyaabse@gmail.com>
2025-09-01 19:42:00 +0300

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+// function greet() {
+//     const name = "Ali"
+//     console.log(`Hello ${name}`)
+// }
+// // great()
+// function greet(name) {
+//     console.log(`Hello ${name}`)
+// }
+// greet("Ali")
+// greet("john")
+// greet("kulani")
+function sum(a,b){
+    const result=a+b
+    return result
+}
+const total=sum(5,6)
+console.log(`total of5&6is:${total}`)
